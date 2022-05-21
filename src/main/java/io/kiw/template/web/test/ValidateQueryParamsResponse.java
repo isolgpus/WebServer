a327e15a2c7e5fd0d6bd5776475e60fa6c11f794
Jamie Stansfield <jalstansfield+github@gmail.com>
2022-05-21 18:24:20 +0100

    Revert "support optionals in map validator"
    This reverts commit 3b1a5a4

--- src/main/java/io/kiw/template/web/test/ValidateQueryParamsResponse.java
+++ src/main/java/io/kiw/template/web/test/ValidateQueryParamsResponse.java
@@ -2,19 +2,13 @@ package io.kiw.template.web.test;
 
 import io.kiw.template.web.infrastructure.JsonResponse;
 
-import java.util.Optional;
-
 public class ValidateQueryParamsResponse implements JsonResponse {
     public final String required;
     public final Integer rangedInt;
-    public final Integer defaultedInt;
-    public final Integer optionalInt;
 
-    public ValidateQueryParamsResponse(String required, Integer rangedInt, Integer defaultedInt, Optional<Integer> optionalInt) {
+    public ValidateQueryParamsResponse(String required, Integer rangedInt) {
 
         this.required = required;
         this.rangedInt = rangedInt;
-        this.defaultedInt = defaultedInt;
-        this.optionalInt = optionalInt.orElse(null);
     }
 }
